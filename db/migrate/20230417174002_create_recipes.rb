661862528fa1c27aafc77d2627bb7f0a96ce988b
sambeck87 <sandro_mipb@hotmail.com>
2023-04-18 19:20:04 -0600

    Add o_delete cascade feature

--- db/migrate/20230417174002_create_recipes.rb
+++ db/migrate/20230417174002_create_recipes.rb
@@ -6,7 +6,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
       t.decimal :cooking_time
       t.text :description
       t.boolean :public, default:false
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
