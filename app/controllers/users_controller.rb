34fb69de2639ad1be59cfc6c3befdd76458de310
sambeck87 <sandro_mipb@hotmail.com>
2023-04-19 16:05:42 -0600

    Add modules_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1 or /users/1.json
-  def show; end
-
   # GET /users/new
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
-  def edit; end
-
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
