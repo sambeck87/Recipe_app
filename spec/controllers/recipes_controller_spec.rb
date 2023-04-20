475370e41184ba30eb13b4b74593f203f9482eb6
sambeck87 <sandro_mipb@hotmail.com>
2023-04-19 18:56:35 -0600

    Add controller tests

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :request do
-  before do
-    @first_user = User.create(name: 'sambeck', email: 'sambeck@outlook.com', encrypted_password: 'todoterreno')
-    @second_user = User.create(name: 'Teofila', email: 'Teofila@outlook.com', encrypted_password: 'mascarita')
-    @recipe = Recipe.create(name: 'sauce', preparation_time: 1, cooking_time: 1, description: 'my sauce recipe', public: true, user_id: @first_user.id)
-    @food = Food.create(name: 'chilli', measurement_unit: 'piece', price: 1, user_id: @first_user.id)
-    @recipe_food = RecipeFood.create(food_id: @food.id, recipe_id: @recipe.id)
-    login_as @first_user
-  end
+  include Warden::Test::Helpers
 
-  after do
-    logout
+  before do
+    @first_user = User.create!(name: 'sambeck', email: 'sambeck@outlook.com', password: 'todoterreno')
+    login_as(@first_user, scope: :user)
+    @recipe = Recipe.create!(name: 'New recipe', preparation_time: 1, cooking_time: 1, description: 'My sauce recipe', public: true, user: @first_user)
   end
 
   describe 'GET #index' do
@@ -27,9 +22,77 @@ RSpec.describe RecipesController, type: :request do
       expect(response).to render_template(:index)
     end
 
-    it 'displays the correct recipe information' do
-      expect(response.body).to include('sauce') # assuming recipe name is displayed
-      expect(response.body).to include('my sauce recipe') # assuming recipe description is displayed
+    it 'displays the correct recipe name' do
+      expect(response.body).to include('New recipe')
+      expect(response.body).to include('My sauce recipe')
+    end
+
+    it 'displays the correct recipe description' do
+      expect(response.body).to include('My sauce recipe')
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      get recipe_path(@recipe.id)
+    end
+
+    it 'displays the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'displays the correct recipe description' do
+      expect(response.body).to include('My sauce recipe')
+    end
+
+    it 'displays all buttons' do
+      expect(response.body).to include('Back to recipes')
+      expect(response.body).to include('Add ingredient')
+      expect(response.body).to include('Generate shopping list')
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      get new_recipe_path
+    end
+
+    it 'displays the correct template' do
+      expect(response).to render_template(:new)
+    end
+
+    it 'returns a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'displays the correct title' do
+      expect(response.body).to include('New recipe')
+    end
+
+    it 'displays the correct tags' do
+      expect(response.body).to include('Name')
+      expect(response.body).to include('Preparation time')
+      expect(response.body).to include('Cooking time')
+      expect(response.body).to include('Description')
+      expect(response.body).to include('Public')
+    end
+
+    it 'displays all buttons' do
+      expect(response.body).to include('Create Recipe')
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before do
+      get recipe_path(@recipe.id)
+    end
+
+    it 'returns a successful response' do
+      expect(response).to be_successful
     end
   end
 end
