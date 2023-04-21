997ddbeac2afd46893d1e92165436f50d767d2b8
sambeck87 <sandro_mipb@hotmail.com>
2023-04-21 09:30:14 -0600

    Change the domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Configure host
-config.action_mailer.default_url_options = { host: 'tudominio.com', protocol: 'https' }
+config.action_mailer.default_url_options = { host: 'https://my-recipes-jx0z.onrender.com', protocol: 'https' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     address:              'smtp-relay.sendinblue.com',
