2e19bb1c694f74191c967e5ced43e94a8f6bb780
Sandro Hernandez <sandro_mipb@hotmail.com>
2023-04-21 18:30:57 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ config.action_mailer.smtp_settings = {
     address:              'smtp-relay.sendinblue.com',
     port:                 587,
     user_name:            'microverstests@gmail.com',
-    password:             '3XpUFO7I5RhAj4a8',
+    password:             ENV['PASSWORD'],
     authentication:       'login',
     enable_starttls_auto: true
 }
